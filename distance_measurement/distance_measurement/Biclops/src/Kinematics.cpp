946674d7f593463acd5eb59ffdac44f1136b121c
ogu <e1052242@soka-u.jp>
2014-04-12 19:12:55 +0900

    New branch for PTU -2

--- distance_measurement/distance_measurement/Biclops/src/Kinematics.cpp
+++
@@ -1,8 +0,0 @@
-#if defined WIN32
-#pragma warning(disable: 4786) // suppresses warning about long identifiers
-#endif
-
-#include "../include/Kinematics.h"
-
-const char *Kinematics::StartToken = "[kinematics]";
-const char *Kinematics::EndToken = "[/kinematics]";
