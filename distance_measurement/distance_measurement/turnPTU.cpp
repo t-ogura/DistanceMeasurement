f336e45e1035a79414efb06c9f348375c0f95847
ogu <e1052242@soka-u.jp>
2014-06-10 17:16:42 +0900

    add turnPTU.cpp and turnPTU.h

---
+++ distance_measurement/distance_measurement/turnPTU.cpp
@@ -0,0 +1,99 @@
+#include "turnPTU.h"
+
+#include <iostream>
+#include <chrono>
+
+PTU::PTU(char* com,int baudrate,unsigned short get_id){
+	set_baud_rate(baudrate);
+	this->COMstream = open_host_port(com);
+	this->id = get_id;
+	int initErrorCode = this->initialize();
+	if (initErrorCode){
+		std::cerr << "Initialize failed [Error code:" << initErrorCode << "]" << std::endl;
+	}
+	this->panAngle = 0;
+	this->tiltAngle = 0;
+}
+
+PTU::~PTU(){}
+
+int PTU::initialize(){
+
+	if (this->COMstream == PORT_NOT_OPENED) return -1;
+	SerialStringOut(COMstream, (unsigned char*)"   ");
+	select_unit(this->id);
+
+	switch (reset_PTU_parser(5000)) {
+	case PTU_OK:
+		break;
+	case PTU_FIRMWARE_VERSION_TOO_LOW:
+		return -2;
+	case PTU_NOT_RESPONDING:
+		return -3;
+	}
+
+	Sleep(2000);
+	
+	const auto start = std::chrono::system_clock::now();
+	unsigned short getBase = 1;
+	set_desired(PAN, BASE, (PTU_PARM_PTR*)&getBase, ABSOLUTE);
+	getBase = 1;
+	set_desired(TILT, BASE, (PTU_PARM_PTR*)&getBase, ABSOLUTE);
+
+	long a = 1;
+	set_desired(PAN, ACCELERATION, (PTU_PARM_PTR*)&a, ABSOLUTE);
+	set_desired(TILT, ACCELERATION, (PTU_PARM_PTR*)&a, ABSOLUTE);
+
+	short low_power = PTU_LOW_POWER;
+	set_desired(PAN, HOLD_POWER_LEVEL, (PTU_PARM_PTR*)&low_power, ABSOLUTE);
+	set_desired(TILT, HOLD_POWER_LEVEL, (PTU_PARM_PTR*)&low_power, ABSOLUTE);
+	set_desired(PAN, MOVE_POWER_LEVEL, (PTU_PARM_PTR*)&low_power, ABSOLUTE);
+	set_desired(TILT, MOVE_POWER_LEVEL, (PTU_PARM_PTR*)&low_power, ABSOLUTE);
+	
+	return 0;
+}
+
+int PTU::turn(double pan, double tilt){
+
+
+	pan *=0.4;
+	tilt *= 0.4;
+	{
+		short panPos = PAN_DEG2STEP(RAD2DEG(pan));
+		short tiltPos = TILT_DEG2STEP(RAD2DEG(tilt));
+		set_desired(PAN, POSITION, (PTU_PARM_PTR *)&panPos, RELATIVE);
+		set_desired(TILT, POSITION, (PTU_PARM_PTR *)&tiltPos, RELATIVE);
+	}
+	return 0;
+}
+
+int PTU::setSpeed(){
+	set_mode(COMMAND_EXECUTION_MODE, EXECUTE_IMMEDIATELY);
+	const auto start = std::chrono::system_clock::now();
+	short vel = SPEED_PAN_MAX / SPEED_FACTOR;
+	set_desired(PAN, SPEED, (PTU_PARM_PTR *)&vel, ABSOLUTE);
+	vel = SPEED_TILT_MAX / SPEED_FACTOR;
+	set_desired(TILT, SPEED, (PTU_PARM_PTR *)&vel, ABSOLUTE);
+	const auto end = std::chrono::system_clock::now();
+
+	return 0;
+}
+
+int PTU::turnHome(){
+	short vel = SPEED_PAN_MAX / 2.;
+	set_desired(PAN, SPEED, (PTU_PARM_PTR *)&vel, ABSOLUTE);
+	vel = SPEED_TILT_MAX / 2.;
+	set_desired(TILT, SPEED, (PTU_PARM_PTR *)&vel, ABSOLUTE);
+	short pos = 0;
+	set_desired(PAN, POSITION, (PTU_PARM_PTR *)&pos, ABSOLUTE);
+	set_desired(TILT, POSITION, (PTU_PARM_PTR *)&pos, ABSOLUTE);
+
+	return 0;
+}
+
+int PTU::getPosition(){
+	this->panAngle = DEG2RAD(PAN_STEP2DEG(get_desired(PAN, POSITION)));
+	this->tiltAngle = DEG2RAD(TILT_STEP2DEG(get_desired(TILT, POSITION)));
+	//std::cout << "pan:" << this->panAngle << " tilt:" << this->tiltAngle << std::endl;
+	return 0;
+}
---
+++ distance_measurement/distance_measurement/turnPTU.h
@@ -0,0 +1,36 @@
+#include "ptu\include\opcodes.h"
+#include "ptu\include\w32seria.h"
+#include "ptu\include\ptu.h"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#define PAN_RES 11109
+#define TILT_RES 2777
+#define PAN_DEG2STEP(X) (-X/(PAN_RES/ 3600. / 60.))
+#define TILT_DEG2STEP(X) (-X/(TILT_RES/ 3600. / 60.))
+#define PAN_STEP2DEG(X) (-X*(PAN_RES/ 3600. / 60.))
+#define TILT_STEP2DEG(X) (-X*(TILT_RES/ 3600. / 60.))
+#define DEG2RAD(X) (X/180.*M_PI)
+#define RAD2DEG(X) (X*180/M_PI)
+
+#define SPEED_PAN_MAX 2801
+#define SPEED_TILT_MAX 2833
+#define SPEED_FACTOR 3
+
+class PTU{
+public:
+	PTU(char* comPort,int baudrate = 9600,unsigned short deviceID = 0);
+	~PTU();
+	int turnHome();
+	int turn(double panAngleRadian, double tiltAngleRadian);
+	int setSpeed();
+	int getPosition();
+
+	double panAngle;
+	double tiltAngle;
+private:
+	int initialize();
+	portstream_fd COMstream;
+	unsigned short id;
+};
